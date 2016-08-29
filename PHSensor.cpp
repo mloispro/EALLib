60322c6c2db6c5b4136a649205da42ef8ebbbd9c
mloispro <olson_mitchell@hotmail.com>
2016-08-29 17:11:04 -0500

    fixed sensor calc

--- PHSensor.cpp
+++ PHSensor.cpp
@@ -67,11 +67,23 @@ double PHSensor::GetPHValue() {
             _pHArrayIndex = 0;
         }
         _voltage = CalculateAverage(_pHAverage, 40) * 5.0 / 1024;
-        float pHVal = 1.5 * _voltage + _offset;
+        //float pHVal = 3.5 * _voltage + _offset; //for DH Robot PH Sensor
+        float pHVal = 7 + ((2.5 - _voltage) / 0.18) + _offset;
         return pHVal;
         samplingTime = millis();
     }
     return _pHValue;
+
+    ////alternate reading
+    //int measure = analogRead(_pin);
+    //_voltage = 5.0 / 1024.0 * measure; //classic digital to voltage conversion
+    ////Serial.print("\tVoltage: ");
+    ////Serial.print(_voltage, 3);
+    //
+    //// PH_step = (voltage@PH7 - voltage@PH4) / (PH7 - PH4)
+    //// PH_probe = PH7 - ((voltage@PH7 - voltage@probe) / PH_step)
+    //float pHVal = 7 + ((2.5 - _voltage) / 0.18);
+    //return pHVal;
 }
 
 double PHSensor::GetVoltage() {
